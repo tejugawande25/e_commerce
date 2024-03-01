78d875fa118edc0353494ec1a6c297a786a708a6
Tejas Gawande <tejugawande25@gmail.com>
2024-03-02 01:18:08 +0530

    items new file

--- e_commerce-frontend/src/components/items/items.js
+++ e_commerce-frontend/src/components/items/items.js
@@ -4,8 +4,11 @@ import "./items.css"
 function Items(){
     return<>
       <div className="items-div">
+        
          <div className="items">
-
+            <img className="item-image" src="https://rukminim2.flixcart.com/image/832/832/xif0q/t-shirt/m/k/l/l-200011-the-souled-store-original-imaggsyfzqx9ekze.jpeg?q=70&crop=true" alt="t-shirt" />
+            <p className="heading-p">The Souled Store</p>
+            <p className="subheading-p">From ₹500</p>
          </div>
          <div className="items">
             
