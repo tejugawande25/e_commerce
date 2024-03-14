c183505067bb7e3f381e59be246e629d2c0bdcdc
Tejas Gawande <tejugawande25@gmail.com>
2024-03-14 14:08:07 +0530

    product section

--- e_commerce-frontend/src/pages/product.js
+++ e_commerce-frontend/src/pages/product.js
@@ -5,7 +5,14 @@ function Product(){
     return<>
     <div className="products">
         <div className="product-section">
-
+            <div className="product-section-heading">
+                Trending T-Shirt's
+            </div>
+            <div className="product-section-div">
+                <div className="product-section-item">
+                    
+                </div>
+            </div>
         </div>
         <div className="product-section">
 
