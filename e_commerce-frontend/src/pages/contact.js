9a176f2cad7f4d48cfe69079e194b414a29c0aa5
Tejas Gawande <tejugawande25@gmail.com>
2024-03-23 18:46:32 +0530

    new changes

--- e_commerce-frontend/src/pages/contact.js
+++ e_commerce-frontend/src/pages/contact.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "./contact.css";
+import CFooter from "../components/c-footer/c-footer";
 
-function Contact(){
-    return <>
-    <h1>This is the contact section.</h1>
+function Contact() {
+  return (
+    <>
+      <div className="contact">
+        <div className="contact-main">
+          <div className="contact-freq"></div>
+          <div className="contact-form">
+            <label>Name: </label>
+            <input type="text" className="username-input" />
+            <label>Contact: </label>
+            <input type="number" className="usercontact-input" />
+
+            <label>Email: </label>
+            <input type="text" className="useremail-input" />
+
+            <label>Enter your problem: </label>
+            <input type="text" className="problem-text" />
+
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </div>
+        </div>
+        <CFooter />
+      </div>
     </>
+  );
 }
 
 export default Contact;
